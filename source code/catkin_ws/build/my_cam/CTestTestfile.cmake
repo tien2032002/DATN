bb7d07bef0a8e4da796db25d2d56bc08378ac493
tien2032002 <80495035+tien2032002@users.noreply.github.com>
2023-09-09 09:42:12 +0700

    init

---
+++ source code/catkin_ws/build/my_cam/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/baotien/catkin_ws/src/my_cam
+# Build directory: /home/baotien/catkin_ws/build/my_cam
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
